0955bf20237e403a9e1c361cbae1d93c9fd79167
zikisacalesogaxa <zikisacalesogaxa@gmail.com>
2020-03-09 10:56:19 +0200

    code refactoring [WIP] 😄.

--- test/yokojack.test.js
+++ test/yokojack.test.js
@@ -4,17 +4,12 @@ const testdata = require ('../data/tests.json');
 
 describe ('yocojack tests', () => {
 
-//   it(`[${testdata[79].playerA}], [${testdata[79].playerB}]`, (done) => {
-//       assert.equal(testdata[79].playerAWins, yocojack(testdata[79]));
-//       done();
-//   });
+    for (let i = 0; i < testdata.length; i++) {
+        const result = testdata[i];
+        it (`player A wins: ${result.playerAWins} on hands [${result.playerA}] v [${result.playerB}]`, done => {
+            assert.equal (result.playerAWins, yocojack (result.playerA, result.playerB));
+            done ();
+        });
+    }
 
-  for (let i = 0; i < testdata.length; i++) {
-    const result = testdata[i];
-    it (`player A wins: ${result.playerAWins} on hands [${result.playerA}] v [${result.playerB}]`, done => {
-      assert.equal (result.playerAWins, yocojack (testdata[i]));
-      done ();
-    });
-  }
-  
 });
